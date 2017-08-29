74f7b9d6bf7a5ca8e2a53e202e5e07f1b32a4325
Tanner <tanner.tannerlevine@gmail.com>
2017-08-29 01:13:20 +0000

    stuck on manipulating user input and adding to database

--- task3/app/models/number.rb
+++ task3/app/models/number.rb
@@ -1,12 +1,11 @@
 class Number < ApplicationRecord
   validates :to_factorial, presence: true, uniqueness: true, length: { maximum: 3 }, numericality: { only_integer: true }
-  before_save :run_factoring, only: [:create]
-
-  private
-
-  def run_factoring
+  
+  before_create do
     factorialed = _factorial(to_factorial, 1)
   end
+
+  private
 
   def _factorial(number, value)
     if number < 2
